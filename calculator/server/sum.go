ecf3d05420e1407edcbcb0ccaff2ec84c312d3d5
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-07 07:43:01 -0300

    Fazendo função calculator do Protobuf

--- calculator/server/sum.go
+++ calculator/server/sum.go
@@ -1,1 +1,16 @@
 package calculator
+
+import (
+	"context"
+	"log"
+
+	"github.com/alvarolucio2007/gRPCPractice/calculator/proto"
+)
+
+func (s *Server) Calculator(ctx context.Context, in *proto.SumRequest) (*proto.SumResponse, error) {
+	log.Printf("Sum function was invoked with %v\n", in)
+	sum := in.FirstNumber + in.SecondNumber
+	return &proto.SumResponse{
+		Result: sum,
+	}, nil
+}
